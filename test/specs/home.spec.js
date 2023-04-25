f2d4652ef702ae19b02b31c6892a2dd24f02e867
MacMini-19 <macmini-19@MacMini-19s-Mac-mini.local>
2023-04-25 15:47:58 -0700

    home

--- test/specs/home.spec.js
+++ test/specs/home.spec.js
@@ -5,4 +5,8 @@ describe ('My Home Page', () => {
         await homePage.open();
         await expect(homePage.listExamples).toBeElementsArrayOfSize({gte: 1});
     });
+    it ('Should fail test by changing assertion number from 44 to 45', async () => {
+        await homePage.open();
+        await !(expect(homePage.listExamples).toBeElementsArrayOfSize(45));
+    });
 });
